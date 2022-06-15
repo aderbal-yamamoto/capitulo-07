2f2fc95b12565e3dc893fdbb6e856f121a899b4f
Aderbal Yamamoto <aderbal_yamamoto@hotmail.com>
2022-06-14 22:07:53 -0300

    Criada class contato Form

--- App/Control/ContatoForm.php
+++ App/Control/ContatoForm.php
@@ -3,86 +3,76 @@ use Livro\Control\Page;
 use Livro\Control\Action;
 use Livro\Widgets\Form\Form;
 use Livro\Widgets\Dialog\Message;
+use Livro\Widgets\Form\Label;
 use Livro\Widgets\Form\Entry;
 use Livro\Widgets\Form\Combo;
 use Livro\Widgets\Form\Text;
 use Livro\Widgets\Wrapper\FormWrapper;
 
-class ContatoForm extends Page
+class ContatoForm extends Page 
 {
     private $form;
-    
+
     function __construct()
     {
         parent::__construct();
         
         // instancia um formulário
         $this->form = new FormWrapper(new Form('form_contato'));
-        $this->form->setTitle('Formulário de contato');
-        
-        // cria os campos do formulário
-        $nome     = new Entry('nome');
-        $email    = new Entry('email');
-        $assunto  = new Combo('assunto');
-        $mensagem = new Text('mensagem');
-        
-        $this->form->addField('Nome', $nome, 300);
-        $this->form->addField('E-mail', $email, 300);
+        $this->form->setTitle('formulario de contato');
+
+        // cria o campo dos formulários 
+        $nome     = new Entry ('nome');
+        $email    = new Entry ('email');
+        $assunto  = new Combo ('assunto');
+        $mensagem = new Text ('mensagem');
+
+        $this->form->addField('Nome', $nome, 300 );
+        $this->form->addField('Email', $email, 300);
         $this->form->addField('Assunto', $assunto, 300);
         $this->form->addField('Mensagem', $mensagem, 300);
-        
+
         // define alguns atributos
-        $assunto->addItems( array('1' => 'Sugestão',
-                                  '2' => 'Reclamação',
-                                  '3' => 'Suporte técnico',
-                                  '4' => 'Cobrança',
-                                  '5' => 'Outro') );
+        $assunto->addItems(array('1' => 'Sugestão',
+                                 '2' => 'Reclamação',
+                                 '3' => 'Suporte Técnico',
+                                 '4' => 'Cobrança',
+                                 '5' => 'Outro') );
         $mensagem->setSize(300, 80);
-        
+
         $this->form->addAction('Enviar', new Action(array($this, 'onSend')));
         
-        // adiciona o formulário na página
+        // adiciona o formulario a pagina
         parent::add($this->form);
     }
-    
-    /**
-     * Envia os dados
-     */
-    function onSend($p)
+    function onSend()
     {
-        try
-        {
-            // obtém os dados
-            $dados = $this->form->getData();
-            
-            // mantém o formulário preenchido
+        try {
+            // obtem os dados
+            $dados = $this->form->getDadta();
+
+            // mantem os dados preenchidos
             $this->form->setData($dados);
-            
+
             // valida
             if (empty($dados->email)) {
-                throw new Exception('Email vazio');
+                throw new Exception("Email vazio");
+                
             }
             if (empty($dados->assunto)) {
-                throw new Exception('Assunto vazio');
+                throw new Exception("assunto vazio");
+                
             }
-            
-            // monta mensagem
-            $mensagem  = "Nome: {$dados->nome} <br>";
+            // monta a mensagem
+            $mensagem = "nome: {$dados->nome} <br>";
             $mensagem .= "Email: {$dados->email} <br>";
             $mensagem .= "Assunto: {$dados->assunto} <br>";
             $mensagem .= "Mensagem: {$dados->mensagem} <br>";
             new Message('info', $mensagem);
+          
+        
+        } catch (Exception $e) {
+            new message('error', $e->getMessage());
         }
-        catch (Exception $e)
-        {
-            new Message('error', $e->getMessage());
-        }
-    }
-    
-    function onLoad()
-    {
-        $obj = new stdClass;
-        $obj->mensagem = 'Escreva aqui o motivo do contato. Seja o mais claro possível...';
-        $this->form->setData($obj);
     }
 }
